3bfd9fea30efbfaf45def3bd3fbcb79a659860b2
Kadahizi <jeanbaptiste.ndagijimana@yourvoice.se>
2019-01-31 17:07:13 +0100

    fixing camera on android

---
+++ src/components/login/allfiles.js
@@ -0,0 +1,18 @@
+export const ILoginErrorData={
+    identifier: ' ',
+    password: ' ',
+    newPassword: ' ',
+    confirmPassword: ' ',
+    email: ' ',
+    smsphone: ' ',
+    pin: ''
+}
+export const ILoginData={
+    identifier: ' ',
+    password: ' ',
+    newPassword: ' ',
+    confirmPassword: ' ',
+    email: ' ',
+    smsphone: ' ',
+    pin: ''
+}
--- src/components/store/actions/user_action.js
+++ src/components/store/actions/user_action.js
@@ -51,14 +51,18 @@ export function LoginUserSuccess(response){
 
 export function loginUser(data){
     return function(dispatch){
-        return axios.post(URL + 'Login',{  identifier: data.identifier.value,
-            password: data.password.value
+        return axios.post(URL + 'Login',{
+            identifier: data.identifier,
+            password: data.password
 
         }).then(response=>{
             //console.log(response);
             const {token} = response.data;
        
-            AsyncStorage.setItem(LocalStorekeys.JWTTOKEN, token).then(()=>{     
+            AsyncStorage.setItem(LocalStorekeys.JWTTOKEN, token).then(()=>{
+                
+                
+                 
             setAuthorizationToken(token);
             let decodeToken = jwtDecode(token);
             //alert(JSON.stringify(decodeToken))
